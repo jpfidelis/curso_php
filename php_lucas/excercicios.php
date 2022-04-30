b060a898da348609ee67650aa1601b38652836ba
jpfidelis <90100834+jpfidelis@users.noreply.github.com>
2022-04-30 11:55:57 -0300

    exercicios

---
+++ php_lucas/excercicios.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios</title>
+</head>
+<body>
+    
+    <?php
+
+    $txt = "Hello World<br>";
+       print  $txt;
+
+
+       $v = array(1,2,3,4,5,6,7,8,9,10);
+        
+       $v1 = 5;
+       VAR_DUMP($v1);
+
+       $v2 = 6.5;
+       VAR_DUMP($v2);
+
+       $v11 = 2; $v12 = 3;
+       $result = $v11+$v12;
+        echo $result
+
+        $v13 = 2.5; $v14 = 5;
+       $result2 = $v13+$v14;
+       echo $result2
+        
+        
+      
+
+    ?>
+
+
+
+   
+
+</body>
+</html>
